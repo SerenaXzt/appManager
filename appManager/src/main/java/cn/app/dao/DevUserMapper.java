05eae31fb45b5349d01365a3a0d40a099362f89f
Administrator <Administrator@192.168.199.162>
2018-03-21 13:47:50 +0800

    已创建pojo模型、mapper接口

---
+++ appManager/src/main/java/cn/app/dao/DevUserMapper.java
@@ -0,0 +1,17 @@
+package cn.app.dao;
+
+import cn.app.pojo.DevUser;
+
+public interface DevUserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(DevUser record);
+
+    int insertSelective(DevUser record);
+
+    DevUser selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(DevUser record);
+
+    int updateByPrimaryKey(DevUser record);
+}
