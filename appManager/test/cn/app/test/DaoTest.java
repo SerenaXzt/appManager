e10bc189939492860b5a9e2397616491aebbe596
Administrator <Administrator@192.168.199.162>
2018-03-22 10:38:02 +0800

    app列表

--- appManager/test/cn/app/test/DaoTest.java
+++ appManager/test/cn/app/test/DaoTest.java
@@ -25,6 +25,10 @@ public class DaoTest {
 		DevUser user = mapper.selectByCodeAndPwd("test001", "123456");
 		System.out.println(user.getDevcode()+"   "+user.getDevname());
 		
+		
+		
+		
+		
 	}
 	
 	
